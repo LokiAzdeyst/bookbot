62d04aeeb05c0af358dc9e8ea49fb543531d0073
Loki Azdeyst <lokiazdeyst@gmail.com>
2025-10-01 18:56:39 -0500

    a description of your changes here

--- main.py
+++ main.py
@@ -1,42 +1,41 @@
-import collections
-def main(file_path):
-    with open(f"{file_path}") as f:
-        file_contents = f.read()
-        return file_contents
+from stats import (
+    get_num_words,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+)
+import sys
 
-def word_count(text):
-    words = text.split()
-    count = 0
-    for word in words:
-        count += 1
-    return count
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+    print(book_path)
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
 
-def count_characters(text):
-    lowered_test = text.lower()
-    count_dic = {}
-    charset = set(lowered_test)
-    for character in charset:
-        count_dic.update({f"{character}" : lowered_test.count(f"{character}")})
-    return count_dic
 
-def sort_on(dic):
-    return len(dic)
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
-if __name__ == '__main__':
-    file_path = "books/frankenstein.txt"
-    #print(f"--- Begin report of {file_path} ---")
-    #print(f"{word_count(main(file_path))} words found in the document")
-    #print()
-    #print()
-    count_dic = count_characters(main(file_path))
-    return_set = set(count_dic)
-    return_list = list(return_set)
-    new_return_list = []
-    for char in return_list:
-        if char.isalpha():
-            new_return_list.append(char)
-    new_return_list.sort()
-    for value_set in new_return_list:
-        if value_set.isalpha():
-            print(f"The \'{value_set}\' character was found {count_dic.get(value_set)} times")
-    #print("--- End report ---")
+
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
+
+
+
+main()
